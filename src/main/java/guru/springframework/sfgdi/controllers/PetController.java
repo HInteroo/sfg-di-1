6aa7338c41ba58798e97b1cb894fd0cf2577f22d
Max <max.juarez625@gmail.com>
2022-02-13 17:20:16 -0500

    PetController error 2 - added @Controller - added Constructor

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,20 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller //added Controller (Assignment)
+
 public class PetController {
 
     private final PetService petService;
+
+    public PetController(PetService petService) { //added constructor (Assignment)
+        this.petService = petService;
+    }
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
